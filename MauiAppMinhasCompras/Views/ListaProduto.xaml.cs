1d3f5ef7794a7e6d78eafc0e08e2b6c24784c734
Luiz Henrique Lima Silva de Oliveira <henriqueluiz607@hotmail.com>
2025-03-09 21:47:19 -0300

    criação da aba de inserção de dados

--- MauiAppMinhasCompras/Views/ListaProduto.xaml.cs
+++ MauiAppMinhasCompras/Views/ListaProduto.xaml.cs
@@ -6,4 +6,16 @@ public partial class ListaProduto : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private async void ToolbarItem_Clicked(object sender, EventArgs e)
+    {
+		try
+		{
+			await Navigation.PushAsync(new Views.NovoProduto());
+
+		} catch (Exception ex)
+		{
+			await DisplayAlert("ishii...", ex.Message, "OK");
+		}
+    }
 }
